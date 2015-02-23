53f686925b8848cc632a613991d17b8c43da32b4
Armend Zeqiraj <armendz@gmail.com>
2015-02-23 20:58:37 +0100

    Sound Manager
    Structure for SoundManager with a blank test case

---
+++ Client/pocketCode/scripts/components/soundManager.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="../../../smartJs/sj.js" />
+/// <reference path="../../../smartJs/sj-event.js" />
+/// <reference path="../core.js" />
+/// <reference path="https://code.createjs.com/createjs-2014.12.12.combined.js" />
+'use strict';
+
+PocketCode.SoundManager = (function () {
+
+    //ctr: sounds = [{id: "s12", name:"asd", url:""}, {...}]
+    function SoundManager(sounds) {	
+		this.init(sounds);
+    }
+
+    //methods
+    SoundManager.prototype.merge({
+        init: function(sounds) {
+			this.createjs
+        },
+
+        startSound: function (id) {
+        },
+
+        pauseSounds: function () {
+        },
+
+        resumeSounds: function () {
+        },
+
+        stopAllSounds: function () {
+        },
+
+        setVolume: function () {
+        },
+		
+        getVolume: function () {
+        },
+    });
+
+    return SoundManager;
+})();
+
+var s = new PocketCode.SoundManager([]);
+//x.int
---
+++ ClientTests/pocketCodeTest/components/test_soundManager.js
@@ -0,0 +1,12 @@
+/// <reference path="../../qunit/qunit-1.16.0.js" />
+'use strict';
+
+QUnit.module("soundManager.js");
+
+
+QUnit.test("SoundManager", function (assert) {
+
+    assert.ok(true, "TODO:");
+
+});
+
