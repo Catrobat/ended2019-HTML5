a3afec1deb599da4c5ca1a2e885369535b55b607
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-03-13 19:12:00 +0100

    added infrastructure for communication layer tests

---
+++ ClientTests/smartJsTest/test_sj-communication.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../qunit/qunit-1.16.0.js" />
+/// <reference path="../../client/smartJs/sj.js" />
+/// <reference path="../../client/smartJs/sj-core.js" />
+/// <reference path="../../client/smartJs/sj-event.js" />
+/// <reference path="../../client/smartJs/sj-components.js" />
+/// <reference path="../../Client/smartJs/sj-communication.js" />
+'use strict';
+
+QUnit.module("sj-communication.js");
+
+QUnit.test("SmartJs.Communication: AJAX", function (assert) {
+
+    assert.ok(true, "TODO: ");
+
+});
+
+QUnit.test("SmartJs.Communication: CORS", function (assert) {
+
+    assert.ok(true, "TODO: ");
+
+});
+
+QUnit.test("SmartJs.Communication: JSONP", function (assert) {
+
+    assert.ok(true, "TODO: ");
+
+});
+
+QUnit.test("SmartJs.Communication: ResourceLoader", function (assert) {
+
+    assert.ok(true, "TODO: ");
+
+});
+
