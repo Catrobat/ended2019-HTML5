5b9e224228f91cd09ff3114a653cca8954928cf8
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-03-21 19:57:13 +0100

    fixes to effect-bricks

--- Server/rest/v0.1/dto/SetBrightnessBrickDto.class.php
+++ Server/rest/v0.1/dto/SetBrightnessBrickDto.class.php
@@ -3,7 +3,7 @@
 class SetBrightnessBrickDto extends SetGraphicEffectBrickDto {
 
   public function __construct($value) {
-		parent::__construct(EGraphicEffect.BRIGHTNESS, $value);
+		parent::__construct(EGraphicEffect::BRIGHTNESS, $value);
 		
 		$this->type = "SetBrightness";
 		
--- Server/rest/v0.1/dto/SetTransparencyBrickDto.class.php
+++ Server/rest/v0.1/dto/SetTransparencyBrickDto.class.php
@@ -4,7 +4,7 @@
 class SetTransparencyBrickDto extends SetGraphicEffectBrickDto {
 
   public function __construct($value) {
-		parent::__construct(EGraphicEffect.GHOST, $value);
+		parent::__construct(EGraphicEffect::GHOST, $value);
 		
 		$this->type = "SetTransparency";
 
