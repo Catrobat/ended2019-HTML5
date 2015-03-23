7d109435cf636dbf94322a32e7adf4306a46734d
MarkusNager <nager.markus.mn@gmail.com>
2015-03-23 16:33:09 +0100

    several tests

--- ClientTests/pocketCodeTest/model/test_sprite.js
+++ ClientTests/pocketCodeTest/model/test_sprite.js
@@ -1,12 +1,51 @@
+/// <reference path="../../../Client/smartJs/sj-event.js" />
+/// <reference path="../../../Client/pocketCode/scripts/model/sprite.js" />
 /// <reference path="../../qunit/qunit-1.16.0.js" />
 'use strict';
 
 QUnit.module("sprite.js");
 
 
-QUnit.test("[missing]", function (assert) {
 
-    assert.ok(true, "TODO:");
+QUnit.test("Sprite", function (assert) {
+
+    var prog= new PocketCode.Model.Program();
+    var sprite = new PocketCode.Model.Sprite(prog,null);
+    assert.ok(sprite instanceof PocketCode.Model.Sprite, "instance check");
+
+
+    assert.throws(sprite.setBrightness("sdfsdf"),false, "invalid brightness percentage");
+    sprite.setBrightness(110);
+    assert.equal(sprite.brightness, 100, "setBrightness over 100");
+
+    sprite.setBrightness(-5);
+    assert.equal(sprite.brightness, 0, "setBrightness under 0");
+
+
+    assert.throws(sprite.changeBrightness("sdfsdf"),false, "invalid brightness value");
+    sprite.setBrightness(90);
+    sprite.changeBrightness(12);
+    assert.equal(sprite.brightness, 100, "changeBrightness over 100");
+
+    sprite.setBrightness(30);
+    sprite.changeBrightness(-32);
+    assert.equal(sprite.brightness, 0, "changeBrightness under 0");
+
+    assert.throws(sprite.setTransparency("sdfsdf"),false, "invalid transparency percentage");
+    sprite.setTransparency(110);
+    assert.equal(sprite.transparency, 100, "setTransparency over 100");
+
+    sprite.setTransparency(-5);
+    assert.equal(sprite.transparency, 0, "setTransparency under 0");
+
+    sprite.setTransparency(90);
+    sprite.changeTransparency(12);
+    assert.equal(sprite.transparency, 100, "setTransparency over 100");
+
+    sprite.setTransparency(30);
+    sprite.changeTransparency(-32);
+    assert.equal(sprite.transparency, 0, "setTransparency under 0");
+
 
 });
 
