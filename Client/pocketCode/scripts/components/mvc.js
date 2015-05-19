d3fa3e6e9f2a3840c116d79df5b9837ca302234a
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-05-19 21:32:20 +0200

    page core classes: mvc

---
+++ Client/pocketCode/scripts/components/mvc.js
@@ -0,0 +1,63 @@
+/// <reference path="../core.js" />
+'use strict';
+
+PocketCode.Mvc = {
+	CoreView: (function () {
+		CoreView.extends(SmartJs.Ui.Control, false);
+	
+		function CoreView(propObject) {
+			SmartJs.Ui.Control.call(this, 'div', propObject);
+			//this._dom = view;
+		
+			this._onHide = new SmartJs.Event.Event(this);
+			//this._viewOnHide = this._view.onHide;
+		}
+
+		//events
+		Object.defineProperties(CoreView.prototype, {
+			onHide: {
+				get: function () { return this._onHide; },
+				//enumerable: false,
+				//configurable: true,
+			},
+		});
+
+		//methods
+		CoreView.prototype.merge({
+			//show: function() {
+			//	//this._view.show();
+			//},
+			/* override */
+			hide: function() {
+				SmartJs.Ui.Control.hide.call(this);	//call super method
+				this._onHide.dispatchEvent();
+			},
+		
+		});
+
+		return CoreView;
+	})(),
+	
+	CoreController: (function () {
+
+		function CoreController(view) {
+			this._view = view;
+
+			//this._viewOnShow = this._view.onShow;
+			//this._viewOnHide = this._view.onHide;
+		}
+
+		CoreController.prototype.merge({
+			show: function () {
+				this._view.show();
+			},
+			hide: function () {
+				this._view.hide();
+			},
+			updateViewState: function (viewState) { },
+		});
+
+		return CoreController;
+	})(),
+};
+
