52faf6baca705630acc5a25a8e091348d388c600
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-08-02 18:49:12 +0200

    tests & fixes: layout and buttons

---
+++ Client/pocketCode/scripts/uiControls/button.js
@@ -0,0 +1,133 @@
+﻿/// <reference path="../../../smartJs/sj.js" />
+/// <reference path="../../../smartJs/sj-core.js" />
+/// <reference path="../../../smartJs/sj-event.js" />
+/// <reference path="../../../smartJs/sj-ui.js" />
+/// <reference path="../core.js" />
+/// <reference path="../ui.js" />
+'use strict';
+
+PocketCode.Ui.Button = (function () {
+    Button.extends(PocketCode.Ui.I18nControl, false);
+
+    //cntr
+    function Button(text, args) {
+        PocketCode.Ui.I18nControl.call(this, 'button', args);
+
+        this._textNode = new SmartJs.Ui.TextNode(text);
+        this._dom.appendChild(this._textNode._dom);
+        //events
+        this._onClick = new SmartJs.Event.Event(this);
+        this._addDomListener(this._dom, 'click', this._clickHandler);
+    }
+
+    //properties
+    Object.defineProperties(Button.prototype, {
+        //icon: {
+        //    get: function () {
+        //        return this._icon;
+        //    },
+        //    set: function (value) {
+        //        this._icon = value;
+
+        //    },
+        //},
+        text: {
+            get: function () {
+                return this._textNode.text;
+            },
+            set: function (value) {
+                this._textNode.text = value;
+            },
+        },
+        disabled: {
+            get: function () {
+                return this._dom.disabled;
+            },
+            set: function (value) {
+                this._dom.disabled = value;
+            },
+        },
+    });
+
+    //events
+    Object.defineProperties(Button.prototype, {
+        onClick: {
+            get: function () {
+                return this._onClick;
+            },
+        },
+    });
+
+    //methods
+    Button.prototype.merge({
+        _clickHandler: function (e) {
+            this._onClick.dispatchEvent();
+        },
+    });
+
+    return Button;
+})();
+
+
+PocketCode.Ui.PlayerSvgButton = (function () {
+    PlayerSvgButton.extends(PocketCode.Ui.Button, false);
+
+    //cntr
+    function PlayerSvgButton(icon, text, big) {
+        PocketCode.Ui.Button.call(this, text, { className: 'pc-playerButton' });
+
+        //this.className = 'pc-playerButton';
+        if (big)
+            this.addClassName('pc-menuBigButton');
+
+        if (!icon)
+            throw new Error('invalid argument: icon');
+        //this._icon = icon;
+        this._textNode = new SmartJs.Ui.TextNode(text);
+        var span = document.createElement('span');
+        span.appendChild(this._textNode._dom);
+        this._dom.innerHTML = icon;//appedChild(icon);
+        this._dom.appendChild(span);
+    }
+
+    //properties
+    Object.defineProperties(PlayerSvgButton.prototype, {
+        //icon: {
+        //    get: function () {
+        //        return this._icon;
+        //    },
+        //    set: function (value) {
+        //        this._icon = value;
+
+        //    },
+        //},
+        //text: {
+        //    get: function () {
+        //        return this._textNode.text;
+        //    },
+        //    set: function (value) {
+        //        this._textNode.text = value;
+        //    },
+        //},
+        checked: {
+            get: function () {
+                var domClasses = this._dom.className.split(/\s+/);
+                var lookupClass = 'pc-playerButtonChecked';
+                for (var i = 0, l = domClasses.length; i < l; i++)
+                    if (domClasses[i] === lookupClass)
+                        return true;
+
+                return false;
+            },
+            set: function (value) {
+                if (value)
+                    this.addClassName('pc-playerButtonChecked');
+                else
+                    this.removeClassName('pc-playerButtonChecked');
+            },
+        },
+    });
+
+    return PlayerSvgButton;
+})();
+
---
+++ ClientTests/pocketCodeTest/_uiControlTests/button/test_button.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+
+window.onload = function () {
+
+    var settingsContainer = document.getElementById('settingsContainer');
+    var outputContainer = document.getElementById('outputContainer');
+    var layoutContainer = document.getElementById('layoutContainer');
+
+    var control = new PocketCode.Ui.Button();
+    layoutContainer.appendChild(control._dom);
+
+    control = new PocketCode.Ui.Button('text');
+    layoutContainer.appendChild(control._dom);
+
+    //click handler
+    var onClickEventFired = function () {
+        outputContainer.innerHTML += '<br />clicked';
+    };
+
+    control.onClick.addEventListener(new SmartJs.Event.EventListener(onClickEventFired, this));
+
+    control.text = 'changed';
+    outputContainer.innerHTML += '<br />text change to: ' + control.text;
+
+    control.disabled = true;
+    outputContainer.innerHTML += '<br />button: disabled: ' + control.disabled;
+
+    control.disabled = false;
+    outputContainer.innerHTML += '<br />button: disabled: ' + control.disabled;
+
+
+    //svg button
+    control = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.PLAY, 'play', true);
+    layoutContainer.appendChild(control._dom);
+    control.onClick.addEventListener(new SmartJs.Event.EventListener(onClickEventFired, this));
+
+    control = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.PAUSE, 'pause', true);
+    layoutContainer.appendChild(control._dom);
+
+    control = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.RESTART, 'restart');
+    layoutContainer.appendChild(control._dom);
+
+    control = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.BACK, 'back');
+    layoutContainer.appendChild(control._dom);
+
+    control = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.SCREENSHOT, 'screenshot');
+    layoutContainer.appendChild(control._dom);
+
+    control = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.AXES, 'axes axes axes');
+    layoutContainer.appendChild(control._dom);
+    control.checked = true;
+    outputContainer.innerHTML += '<br />svg button: checked: ' + control.checked;
+    control.checked = false;
+    outputContainer.innerHTML += '<br />svg button: checked: ' + control.checked;
+
+    control.disabled = true;
+    outputContainer.innerHTML += '<br />svg button: disabled: ' + control.disabled;
+
+    control.disabled = false;
+    outputContainer.innerHTML += '<br />svg button: disabled: ' + control.disabled;
+
+
+};
