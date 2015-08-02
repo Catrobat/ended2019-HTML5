b909b1f4a727fe8fc0093f68c7630acf68a558f3
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-08-03 00:26:54 +0200

    playerToolbar + test page (initial)
    control based on finished mockup.. work in progress

--- Client/pocketCode/scripts/uiControls/playerToolbar.js
+++ Client/pocketCode/scripts/uiControls/playerToolbar.js
@@ -16,39 +16,70 @@ PocketCode.Ui.merge({
     showOnGesture: indicates if the menu is bound to a page event to show it based on gesture events = for IOS devices (no back button)
     */
     PlayerToolbarSettings: {
-        DESKTOP: { orientation: 'h', position: 'lo', hideable: false, animatedShowHide: false, hideOnStart: false, showOnGesture: false },
-        MOBILE: { orientation: 'v', position: 'bi', hideable: true, animatedShowHide: true, hideOnStart: false, showOnGesture: false },
+        DESKTOP: { orientation: 'v', position: 'lo', hideable: false, animatedShowHide: false, hideOnStart: false, showOnGesture: false },
+        MOBILE: { orientation: 'h', position: 'bi', hideable: true, animatedShowHide: true, hideOnStart: false, showOnGesture: false },
         MOBILE_IOS: { orientation: 'v', position: 'bi', hideable: true, animatedShowHide: true, hideOnStart: true, showOnGesture: true },
     },
 
-    PlayerEventArguments: {
-        BACK: { command: 2, },
-        RESTART: { command: 3, },
-        PLAY: { command: 4, },
-        PAUSE: { command: 5, },
-        SCREENSHOT: { command: 6, },
-        AXES: { command: 7, },
+    PlayerBtnCommand: {
+        BACK: 2,//{ command: 2, },
+        RESTART: 3,//{ command: 3, },
+        PLAY: 4,//{ command: 4, },
+        PAUSE: 5,//{ command: 5, },
+        SCREENSHOT: 6,//{ command: 6, },
+        AXES: 7,//{ command: 7, },
     },
 
     PlayerToolbar: (function () {
         PlayerToolbar.extends(PocketCode.Ui.I18nControl, false);
 
         function PlayerToolbar(settings) {
-            var div = document.createElement('div');
-            var innerContainer = div;
+            if (!settings || !settings.orientation)
+                throw new Error('invalif argument: cntr settings');
+
+            PocketCode.Ui.I18nControl.call(this, 'div', { className: 'pc-playerMenu' + settings.orientation.toUpperCase() + ' pc-overlay' });
+
+            //internal settings (for scaling)
+            this._defaultHeight = 455;  //all buttons at 10px font-size
+            this._defaultWidth = 371;
 
             if (settings.position == 'bi') { //with green background
-                innerContainer = this._createMenuBackground(div);
+                this._overlay = new SmartJs.Ui.Control('div', { className: 'pc-playerMenuOverlay' });
+                this._overlayBtn = new SmartJs.Ui.Control('div', { className: 'pc-playerMenuPlayOverlay' });
+                this._overlayBtn._dom.appendChild(document.createElement('div'));
+
+                this._appendChild(this._overlay);
+                this._appendChild(this._overlayBtn);
             }
 
-            if (settings.position == 'lo' || settings.position == 'bi') {
-                this._createMenuButtons(innerContainer);
+            this._menuContainer = new SmartJs.Ui.ContainerControl('div', { className: 'pc-playerMenuContainer' });
+            if (settings.position == 'lo') {
+                //TODO: check for bug: this._menuContainer.addClassName('pc-playerMenuOutside');
+                this._menuContainer.className + 'pc-playerMenuContainer pc-playerMenuOutside';
             }
-            else
-                throw new Error('Unsupported setting: position');
+            this._menuContainerAlign = new SmartJs.Ui.ContainerControl('div', { className: 'pc-alignedCell' });
+            this._menuContainer.appendChild(this._menuContainerAlign);
+            this._appendChild(this._menuContainer);
 
-            //base ctr is called after creating the menus base container
-            PocketCode.Ui.I18nControl.call(this, div);
+            //btns
+            this._backButton = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.BACK, 'Back');
+            this._backButton.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this.onButtonClicked.dispatchEvent({ command: PocketCode.Ui.PlayerBtnCommand.BACK }); }, this));
+            this._menuContainerAlign.appendChild(this._backButton);
+            this._restartButton = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.RESTART, 'Restart');
+            this._restartButton.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this.onButtonClicked.dispatchEvent({ command: PocketCode.Ui.PlayerBtnCommand.RESTART }); }, this));
+            this._menuContainerAlign.appendChild(this._restartButton);
+            this._playButton = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.PLAY, 'Play', true);
+            this._playButton.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this.onButtonClicked.dispatchEvent({ command: PocketCode.Ui.PlayerBtnCommand.PLAY }); }, this));
+            this._menuContainerAlign.appendChild(this._playButton);
+            this._pauseButton = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.PAUSE, 'Pause', true);
+            this._pauseButton.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this.onButtonClicked.dispatchEvent({ command: PocketCode.Ui.PlayerBtnCommand.PAUSE }); }, this));
+            this._menuContainerAlign.appendChild(this._pauseButton);
+            this._screenshotButton = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.SCREENSHOT, 'Screenshot');
+            this._screenshotButton.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this.onButtonClicked.dispatchEvent({ command: PocketCode.Ui.PlayerBtnCommand.SCREENSHOT }); }, this));
+            this._menuContainerAlign.appendChild(this._screenshotButton);
+            this._axesButton = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.AXES, 'Axes');
+            this._axesButton.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this.onButtonClicked.dispatchEvent({ command: PocketCode.Ui.PlayerBtnCommand.AXES }); }, this));
+            this._menuContainerAlign.appendChild(this._axesButton);
 
             this.executionState = PocketCode.ExecutionState.STOPPED;
             this.onResize.addEventListener(new SmartJs.Event.EventListener(this._resizeHandler, this));
@@ -60,10 +91,25 @@ PocketCode.Ui.merge({
         Object.defineProperties(PlayerToolbar.prototype, {
             executionState: {
                 set: function (value) {
-                    this._executionState = value;
-                    this._updateExecutionState();
+                    //this._executionState = value;
+                    //this._updateExecutionState();
+                    switch (value) {
+                        case PocketCode.ExecutionState.RUNNING:
+                            this._playButton.hide();
+                            this._pauseButton.show();
+                            break;
+                        default:
+                            this._playButton.show();
+                            this._pauseButton.hide();
+                            break;
+                    }
                 },
-            }
+            },
+            axesButtonChecked: {
+                set: function (value) {
+                    this._axesButton.checked = value;
+                },
+            },
         });
 
         //events
@@ -77,26 +123,33 @@ PocketCode.Ui.merge({
 
         //methods
         PlayerToolbar.prototype.merge({
+            /* override */
             hide: function() {
-                this.hideBrowseProjectsButton();
+                //this.hideBrowseProjectsButton();
                 SmartJs.Ui.Control.prototype.hide.call(this);
             },
-            showBrowseProjectsButton: function () {
-                //shows a button "browse other projects" to be called on project onExecuted event
+            /* override */
+            show: function() {
+                //this.hideBrowseProjectsButton();
+                SmartJs.Ui.Control.prototype.hide.call(this);
             },
-            hideBrowseProjectsButton: function() {
+            //showBrowseProjectsButton: function () {
+            //    //shows a button "browse other projects" to be called on project onExecuted event
+            //},
+            //hideBrowseProjectsButton: function() {
 
-            },
-            _createMenuBackground: function (container) {
-                return container;   //TODO: 
-                //returns inner container where the buttons have to be placed in
-            },
-            _createMenuButtons: function(container) {
+            //},
+            //_createMenuBackground: function (container) {
+            //    return container;   //TODO: 
+            //    //returns inner container where the buttons have to be placed in
+            //},
+            //_createMenuButtons: function(container) {
 
-            },
-            _updateExecutionState: function () {
-                //show text start/resume and button start or Pause
-            },
+            //},
+            //_updateExecutionState: function () {
+
+            //    //show text start/resume and button start or Pause
+            //},
             _resizeHandler: function (args) {
 
             },
---
+++ ClientTests/pocketCodeTest/_uiControlTests/playerToolbar/test_playerToolbar.js
@@ -0,0 +1,71 @@
+﻿/// <reference path="../../../../Client/pocketCode/scripts/uiControls/playerToolbar.js" />
+'use strict';
+
+window.onload = function () {
+
+    var settingsContainer = document.getElementById('settingsContainer');
+    var outputContainer = document.getElementById('outputContainer');
+    var layoutContainer = document.getElementById('layoutContainer');
+
+    var tbButton1 = new PocketCode.Ui.Button('cancel');
+    var tbButton2 = new PocketCode.Ui.Button('ok');
+    var tbButton3 = new PocketCode.Ui.Button('not supported');
+
+    var control = new PocketCode.Ui.PlayerToolbar(PocketCode.Ui.PlayerToolbarSettings.DESKTOP);
+
+    //click handler
+    var onClickEventFired = function (e) {
+        outputContainer.innerHTML += '<br />clicked: ' + e.command;
+    };
+
+    control.onButtonClicked.addEventListener(new SmartJs.Event.EventListener(onClickEventFired, this));
+
+    //window.onresize = function (e) {
+    //    control._onResize.dispatchEvent();
+    //};
+    //control._onResize.dispatchEvent();  //once at the beginning
+
+    layoutContainer.appendChild(control._dom);
+
+    //control.caption = 'some text (header)';
+    //outputContainer.innerHTML += '<br />added caption: ' + control.caption;
+
+    //control.type = PocketCode.DialogType.WARNING;
+    //outputContainer.innerHTML += '<br />changed type to: ' + control.type;
+
+    //control.type = PocketCode.DialogType.ERROR;
+    //outputContainer.innerHTML += '<br />changed type to: ' + control.type;
+
+    //control.type = PocketCode.DialogType.DEFAULT;
+    //outputContainer.innerHTML += '<br />changed type to: ' + control.type;
+
+    ////add buttons
+    //control.addButton(tbButton1);
+    //control.addButton(tbButton2);
+    //try {
+    //    control.addButton(tbButton3); //3rd button throws error
+    //}
+    //catch (e) {
+    //    outputContainer.innerHTML += '<br />error: 3rd button';
+    //}
+
+    //try {
+    //    control.addButton(document.createElement('div')); //3rd button throws error
+    //}
+    //catch (e) {
+    //    outputContainer.innerHTML += '<br />error: button type';
+    //}
+
+    ////add text to body
+    //var text = 'some text to test the layout.. schould be long enough to salidate new lines'
+    //control.bodyInnerHTML = text;
+    //control.bodyInnerHTML += text;
+    //control.bodyInnerHTML += text;
+    //control.bodyInnerHTML += text;
+
+    //if (control.bodyInnerHtml === text)
+    //    outputContainer.innerHTML += '<br />set body text';
+    //else
+    //    outputContainer.innerHTML += '<br />error: invalid body text ???';
+
+};
