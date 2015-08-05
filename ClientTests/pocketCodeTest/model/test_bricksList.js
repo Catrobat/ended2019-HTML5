869ced771acf0a3808769230a0c40a7d3b0d99c4
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-08-06 01:55:10 +0200

    implementation + tests: list-bricks (DONE)
    finished tests

--- ClientTests/pocketCodeTest/model/test_bricksList.js
+++ ClientTests/pocketCodeTest/model/test_bricksList.js
@@ -26,7 +26,7 @@ QUnit.test("AppendToListBrick", function (assert) {
     assert.ok(b.objClassName === "AppendToListBrick", "objClassName check");
 
     //check value
-    assert.equal(b._value.calculate(), 1.0, "formual calculation (value to set)");
+    assert.equal(b._value.calculate(), 1.0, "formula calculation (value to set)");
 
     //execute
     var executedHandler = function (e) {
@@ -34,7 +34,8 @@ QUnit.test("AppendToListBrick", function (assert) {
         var loopDelay = e.loopDelay ? e.loopDelay : false;
         assert.equal(loopDelay, false, "loop delay check");
 
-        //TODO: assert.equal(sprite.getVariable("var1").value, 1.0, "variable set correctly (local)");
+        var list = sprite.getList("var1");
+        assert.equal(list.valueAt(4), 1.0, "list append");
     };
 
     b.execute(new SmartJs.Event.EventListener(executedHandler, this), "AppendToList");
@@ -59,18 +60,20 @@ QUnit.test("InsertAtListBrick", function (assert) {
     assert.ok(b.objClassName === "InsertAtListBrick", "objClassName check");
 
     //check value
-    assert.equal(b._value.calculate(), 1.0, "formual calculation (value to set)");
+    assert.equal(b._value.calculate(), 1.0, "formula calculation (value to set)");
 
     //execute
     var executedHandler = function (e) {
-        assert.equal(e.id, "AppendToList", "return id check");
+        assert.equal(e.id, "InsertAt", "return id check");
         var loopDelay = e.loopDelay ? e.loopDelay : false;
         assert.equal(loopDelay, false, "loop delay check");
 
-        //TODO: assert.equal(sprite.getVariable("var1").value, 1.0, "variable set correctly (local)");
+        var list = sprite.getList("var1");
+        assert.equal(list.valueAt(2), 1.0, "list insert");
+        assert.equal(list.length, 4, "list length after insert");
     };
 
-    b.execute(new SmartJs.Event.EventListener(executedHandler, this), "AppendToList");
+    b.execute(new SmartJs.Event.EventListener(executedHandler, this), "InsertAt");
 
 });
 
@@ -84,26 +87,28 @@ QUnit.test("ReplaceAtListBrick", function (assert) {
     sprite._lists = [{ id: "var1", name: "var1name", value: [0, 1, "2"] }, { id: "var2", name: "var2name", value: [3, "4", 5] }, ];
 
     var value = JSON.parse('{"type":"NUMBER","value":"1.0","right":null,"left":null}');
-    var b = new PocketCode.Bricks.ReplaceAtListBrick("device", sprite, { referenceId: "var1", index: 2, value: value });
+    var b = new PocketCode.Bricks.ReplaceAtListBrick("device", sprite, { referenceId: "var1", index: 3, value: value });
 
-    assert.ok(b._device === "device" && b._sprite instanceof PocketCode.Sprite && b._list === sprite.getList("var1") && b._idx == 2 && b._value instanceof PocketCode.Formula, "brick created and properties set correctly");
+    assert.ok(b._device === "device" && b._sprite instanceof PocketCode.Sprite && b._list === sprite.getList("var1") && b._idx == 3 && b._value instanceof PocketCode.Formula, "brick created and properties set correctly");
     assert.ok(b._list instanceof PocketCode.Model.UserVariableList, "variable found: type ok");
     assert.ok(b instanceof PocketCode.Bricks.ReplaceAtListBrick, "instance check");
     assert.ok(b.objClassName === "ReplaceAtListBrick", "objClassName check");
 
     //check value
-    assert.equal(b._value.calculate(), 1.0, "formual calculation (value to set)");
+    assert.equal(b._value.calculate(), 1.0, "formula calculation (value to set)");
 
     //execute
     var executedHandler = function (e) {
-        assert.equal(e.id, "AppendToList", "return id check");
+        assert.equal(e.id, "ReplaceAt", "return id check");
         var loopDelay = e.loopDelay ? e.loopDelay : false;
         assert.equal(loopDelay, false, "loop delay check");
 
-        //TODO: assert.equal(sprite.getVariable("var1").value, 1.0, "variable set correctly (local)");
+        var list = sprite.getList("var1");
+        assert.equal(list.valueAt(3), 1.0, "list replace");
+        assert.equal(list.length, 3, "list length after replace");
     };
 
-    b.execute(new SmartJs.Event.EventListener(executedHandler, this), "AppendToList");
+    b.execute(new SmartJs.Event.EventListener(executedHandler, this), "ReplaceAt");
 
 });
 
@@ -124,7 +129,18 @@ QUnit.test("DeleteAtListBrick", function (assert) {
     assert.ok(b.objClassName === "DeleteAtListBrick", "objClassName check");
 
     //execute
+    var executedHandler = function (e) {
+        assert.equal(e.id, "DeleteAt", "return id check");
+        var loopDelay = e.loopDelay ? e.loopDelay : false;
+        assert.equal(loopDelay, false, "loop delay check");
 
+        var list = sprite.getList("var1");
+        assert.equal(list.valueAt(1), 0, "list delete: 1st item");
+        assert.equal(list.valueAt(2), 2, "list delete: 2nd item: moved 3rd to 2nd (+ cast to number)");
+        assert.equal(list.length, 2, "list length after delete");
+    };
+
+    b.execute(new SmartJs.Event.EventListener(executedHandler, this), "DeleteAt");
 
 });
 
