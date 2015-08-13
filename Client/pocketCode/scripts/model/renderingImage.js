66cf2218f2dd9f79b1fe51680346d8aa2cad40da
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-08-13 19:15:09 +0200

    renamed and extended: renderingItem

--- Client/pocketCode/scripts/model/renderingImage.js
+++ Client/pocketCode/scripts/model/renderingImage.js
@@ -1,17 +1,10 @@
-﻿/// <reference path="../core.js" />
-'use strict';
+﻿
+PocketCode.Model.RenderingImage = (function () {
+    RenderingImage.extends(fabric.util.createClass(fabric.Image, {
+        //type: 'sprite',
 
-
-/**
- * @namespace CV
- * @type {{}|*}
- */
-
-PocketCode.Model.RenderingImage = fabric.util.createClass(fabric.Image, {
-        type: 'sprite',
-
-        initialize: function(element, options) {
-            options || (options = { });
+        initialize: function (element, options) {
+            options || (options = {});
 
             this.callSuper('initialize', element, options);
 
@@ -25,42 +18,66 @@ PocketCode.Model.RenderingImage = fabric.util.createClass(fabric.Image, {
                 hasRotatingPoint: false,
                 originX: "center",
                 originY: "center",
-                positionX:options.x,
-                positionY:options.y,
-                filters:[],
-                opacity:1.0
+                positionX: options.x,
+                positionY: options.y,
+                //flipX = flipH: false, //already a property and false (default)
+                //flipy = flipV: false, //already a property and false (default)
+                filters: [],
+                opacity: 1.0
             });
 
             this.setAngle(options.angle);
             this.setOpacity(options.opacity);
         },
 
-        toObject: function() {
+        toObject: function () {
             return fabric.util.object.extend(this.callSuper('toObject'), {
                 id: this.get('id'),
                 name: this.get('name')
             });
         },
 
-        _render: function(ctx) {
+        _render: function (ctx) {
             this.callSuper('_render', ctx);
         },
+    }));
 
-        setAngle: function(direction){
+    function RenderingImage() {
+        this.type = 'sprite';
+        //this.set({
+        //    id: options.id,
+        //    name: options.name,
+        //    perPixelTargetFind: true, // only pixels inside item area trigger click
+        //    selectable: false,
+        //    hasControls: false,
+        //    hasBorders: false,
+        //    hasRotatingPoint: false,
+        //    originX: "center",
+        //    originY: "center",
+        //    positionX: options.x,
+        //    positionY: options.y,
+        //    flipH: false,
+        //    flipV: false,
+        //    filters: [],
+        //    opacity: 1.0
+        //});
+    }
+
+    //methods
+    RenderingImage.prototype.merge({
+        setAngle: function (direction) {
             this.angle = direction - 90;
         },
-
-        setOpacity: function(transparency){
+        setOpacity: function (transparency) {
             this.opacity = +(1 - transparency / 100).toFixed(2);
         },
-
-        applyBrightness: function(brightness){
-            var bright = +((255/100)*(brightness - 100)).toFixed(0);
-            var brightnessFilter = new fabric.Image.filters.Brightness({brightness: bright});
+        applyBrightness: function (brightness) {
+            var bright = +((255 / 100) * (brightness - 100)).toFixed(0);
+            var brightnessFilter = new fabric.Image.filters.Brightness({ brightness: bright });
 
             var overwriteFilter = false;
-            for (var i = 0; i < this.filters.length; i++){
-                if (this.filters[i].type == "Brightness"){
+            for (var i = 0; i < this.filters.length; i++) {
+                if (this.filters[i].type == "Brightness") {
                     this.filters[i] = brightnessFilter;
                     overwriteFilter = true;
                 }
@@ -72,7 +89,7 @@ PocketCode.Model.RenderingImage = fabric.util.createClass(fabric.Image, {
             var replacement = fabric.util.createImage();
             var imgEl = this._originalElement;
             var canvasEl = fabric.util.createCanvasElement();
-            var  _this = this;
+            var _this = this;
 
             canvasEl.width = imgEl.width;
             canvasEl.height = imgEl.height;
@@ -88,5 +105,7 @@ PocketCode.Model.RenderingImage = fabric.util.createClass(fabric.Image, {
             _this._filteredEl = replacement;
             replacement.src = canvasEl.toDataURL('image/png');
         }
-
     });
+
+    return RenderingImage;
+})();
