188890435ad5d8d0f6c2f30303576952b10bbc07
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-08-13 19:13:50 +0200

    views (new)
    definition of views used in the player app

---
+++ Client/pocketCode/scripts/view/pageView.js
@@ -0,0 +1,73 @@
+﻿/// <reference path="../../../smartJs/sj.js" />
+/// <reference path="../../../smartJs/sj-event.js" />
+/// <reference path="../../../smartJs/sj-core.js" />
+/// <reference path="../../../smartJs/sj-components.js" />
+/// <reference path="../../../smartJs/sj-ui.js" />
+/// <reference path="../ui.js" />
+'use strict';
+
+
+PocketCode.Ui.PageView = (function () {
+    PageView.extends(SmartJs.Ui.ContainerControl, false);
+
+    //cntr
+    function PageView(propObject) {
+        SmartJs.Ui.ContainerControl.call(this, { className: 'pc-page' }.merge(propObject));
+
+        this._header = new SmartJs.Ui.ContainerControl({ className: 'pc-pageHeader' });
+        this._appendChild(this._header);
+        this._captionArea = new SmartJs.Ui.ContainerControl();
+        this._caption = new SmartJs.Ui.TextNode();  //TODO: not needed right now
+        this._captionArea.appendChild(this._caption);
+
+        //define body as inner container: override
+        this._container = new SmartJs.Ui.ContainerControl({ className: 'pc-pageBody' });  //TODO: style really needed?
+        this._appendChild(this._container);
+        this._footer = new SmartJs.Ui.ContainerControl({ className: 'pc-pageFooter' });
+        this.appendChild(this._footer);
+
+        this.onResize.addEventListener(new SmartJs.Event.EventListener(this._handleResize, this));
+    }
+
+    //properties
+    Object.defineProperties(PageView.prototype, {
+        //caption: {  //TODO: not needed right now
+        //    get: function () {
+        //        return this._caption.text;
+        //    },
+        //    set: function (value) {
+        //        this._caption.text = value;
+        //    },
+        //}
+    });
+
+    //events
+    Object.defineProperties(PageView.prototype, {
+    });
+
+    //methods
+    PageView.prototype.merge({
+        _handleResize: function(e) {
+            //TODO: make sure to edit the caption area as well to enable text-overflow: ellipsis (max-width)
+            this._container.height = this.height - this._header.height - this._footer.height;
+        },
+        hideHeader: function () {
+            this._header.hide();
+            this._handleResize();
+        },
+        showHeader: function () {
+            this._header.show();
+            this._handleResize();
+        },
+        hideFooter: function () {
+            this._footer.hide();
+            this._handleResize();
+        },
+        showFooter: function () {
+            this._footer.show();
+            this._handleResize();
+        },
+    });
+
+    return PageView;
+})();
---
+++ Client/pocketCode/scripts/view/playerPageView.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../core.js" />
+/// <reference path="../components/gameEngine.js" />
+/// <reference path="../ui/playerStartScreen.js" />
+/// <reference path="../ui/playerToolbar.js" />
+/// <reference path="../controller/programViewportController.js" />
+'use strict';
+
+PocketCode.Ui.PlayerPageView = (function () {
+    PlayerPageView.extends(PocketCode.Ui.PageView);
+
+    function PlayerPageView() {
+
+        this._header.hide();
+        this._footer.hide();
+
+        this._viewport = new PocketCode.PlayerViewportController(); //TODO: a controller instance in a view?
+        //this._toolbar = new PocketCode.Ui.PlayerToolbar();    //TODO: ctr settings
+        this._startScreen = new PocketCode.Ui.PlayerStartScreen();
+
+        this._initPageLayout();
+    }
+
+    PlayerPageView.prototype.merge({
+        _initPageLayout: function() {
+            //this.appendChild(this._viewport); //TODO: add a view and not the controller
+            //this.appendChild(this._toolbar);
+            this.appendChild(this._startScreen);
+        },
+        /* override */    //this is a test->remove this
+        show: function () {
+            //?
+        },
+    });
+
+    return PlayerPageView;
+})();
