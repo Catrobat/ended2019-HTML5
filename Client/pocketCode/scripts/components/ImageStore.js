d66d78c1f9f12670061e17b3d33d57ef2876e6ea
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2015-08-15 16:35:11 +0200

    image store definition added

---
+++ Client/pocketCode/scripts/components/ImageStore.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../../../smartJs/sj.js" />
+/// <reference path="../../../smartJs/sj-core.js" />
+'use strict';
+
+/*
+ * this class is used inside the gameEngine to load, cache, .. images + do some pre-optimizations (e.g. trim)
+ */
+PocketCode.ImageStore = (function () {
+    ImageStore.extends(SmartJs.Core.EventTarget);
+
+    //ctr
+    function ImageStore(scope, globalLookupHost) {
+    }
+
+    //properties
+    Object.defineProperties(ImageStore.prototype, {
+    });
+
+    //events
+    Object.defineProperties(ImageStore.prototype, {
+    });
+
+    //methods
+    ImageStore.prototype.merge({
+    });
+
+    return ImageStore;
+})();
