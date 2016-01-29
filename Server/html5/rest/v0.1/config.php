b6e8f113babf237a9446b3b66de97da470d72e5b
Marc Schober <schomarc@gmail.com>
2016-01-29 16:07:25 +0100

    LoggingController: changed config

--- Server/html5/rest/v0.1/config.php
+++ Server/html5/rest/v0.1/config.php
@@ -1,9 +1,9 @@
 <?php
 define("EMAIL_SMTP", "smtp.gmail.com");
 define("EMAIL_SMTP_PORT", 465);
-define("EMAIL_FROM", "schomarc@gmail.com");
+define("EMAIL_FROM", "email@gmail.com");
 define("EMAIL_NAME_FROM", "Marc Schober");
-define("EMAIL_TO", "schomarc@gmail.com");
+define("EMAIL_TO", "email@gmail.com");
 define("EMAIL_NAME_TO", "Marc Schober");
-define("EMAIL_USER", "schomarc@gmail.com");
-define("EMAIL_PWD", "nokia@al1721");
+define("EMAIL_USER", "email@gmail.com");
+define("EMAIL_PWD", "password");
