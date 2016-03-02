7e070a873e0331141b2afcfec52ea2206ec5ceaf
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2016-03-02 16:02:51 +0100

    please do NOT push debug changes as this will cause our unit tests failing

--- Client/pocketCode/scripts/components/userVariableHost.js
+++ Client/pocketCode/scripts/components/userVariableHost.js
@@ -39,7 +39,7 @@ PocketCode.UserVariableHost = (function () {
                     vars = this.__variablesSimple.getVariables();
                 for (var v in vars) {   //{[id]: {}}
                     if (vars.hasOwnProperty(v))
-                        list.push({ id: v, text: vars[v].value || '', x: 0, y: 0, visible: true }); //TEST ONLY: SET VISIBILITX TO FALSE (DEFAULT)
+                        list.push({ id: v, text: vars[v].value || '', x: 0, y: 0, visible: false });
                 }
                 return list;
             },
