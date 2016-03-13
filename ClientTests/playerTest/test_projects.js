492b8ee953c413967958a8c383df72b7b6732575
Michael Pittner <mpittner@student.tugraz.at>
2016-03-13 16:29:59 +0100

    add object for server errors and object for client errors - skipped if listed on testing

--- ClientTests/playerTest/test_projects.js
+++ ClientTests/playerTest/test_projects.js
@@ -53,6 +53,9 @@ QUnit.test("[missing]", function (assert) {
   /* ************************************************************************* */
   /* ************************************************************************* */
 
+  if( test_only_listed_programs == true ) {
+    limit = Object.keys(server_known_errors).length;
+  }
 
   // init
   var i = 0;
@@ -78,43 +81,70 @@ QUnit.test("[missing]", function (assert) {
   assert.ok( true, "Try to test " + limit_txt + " projects" );
 
 
-  // Fetch a list of Projects and save them to "receivedObject"
-  var url = PocketCode.Services.PROJECT_SEARCH;
-  var srAllProjects = new PocketCode.ServiceRequest(url, SmartJs.RequestMethod.GET, {limit: limit, mask: "downloads"});
+  if( test_only_listed_programs == false ) {
+    // Fetch a list of Projects and save them to "receivedObject"
+    var url = PocketCode.Services.PROJECT_SEARCH;
+    var srAllProjects = new PocketCode.ServiceRequest(url, SmartJs.RequestMethod.GET, {
+      limit: limit,
+      mask: "downloads"
+    });
 
 
-  var onSuccessProjectsHandler = function (e) {
-    receivedObject = e.responseJson;
-    //var allProjectsCount = receivedObject.items.length;
-    var allProjectsCount = receivedObject.totalProjects;
+    var onSuccessProjectsHandler = function (e) {
+      receivedObject = e.responseJson;
+      //var allProjectsCount = receivedObject.items.length;
+      var allProjectsCount = receivedObject.totalProjects;
 
-    if( limit == 0 ) {
-      // resend request to test all Projects
-      limit = allProjectsCount;
-      for (i = 0; i < limit; i++) {
-        done[i+1] = assert.async();
+      if (limit == 0) {
+        // resend request to test all Projects
+        limit = allProjectsCount;
+        for (i = 0; i < limit; i++) {
+          done[i + 1] = assert.async();
+        }
+        console.log(limit);
+        srAllProjects = new PocketCode.ServiceRequest(url, SmartJs.RequestMethod.GET, {
+          limit: limit,
+          mask: "downloads"
+        });
+        srAllProjects.onLoad.addEventListener(new SmartJs.Event.EventListener(onSuccessProjectsHandler, this));
+        srAllProjects.onError.addEventListener(new SmartJs.Event.EventListener(onErrorProjectsHandler, this));
+        PocketCode.Proxy.send(srAllProjects);
+      } else {
+        assert.ok(true, "get List of " + limit + " projects (total Projects: " + allProjectsCount + " / test " + limit + " projects)");
+        getSingleTestProject();
       }
-      console.log( limit );
-      srAllProjects = new PocketCode.ServiceRequest(url, SmartJs.RequestMethod.GET, {limit: limit, mask: "downloads"});
-      srAllProjects.onLoad.addEventListener(new SmartJs.Event.EventListener(onSuccessProjectsHandler, this));
-      srAllProjects.onError.addEventListener(new SmartJs.Event.EventListener(onErrorProjectsHandler, this));
-      PocketCode.Proxy.send(srAllProjects);
-    } else {
-      assert.ok(true, "get List of " + limit + " projects (total Projects: " + allProjectsCount +  " / test " + limit + " projects)");
-      getSingleTestProject();
+    };
+
+    var onErrorProjectsHandler = function (e) {
+      console.log("---- ERROR ----");
+      console.log(e);
+      assert.ok(true, "Fetch of all projects failed");
+    };
+
+    srAllProjects.onLoad.addEventListener(new SmartJs.Event.EventListener(onSuccessProjectsHandler, this));
+    srAllProjects.onError.addEventListener(new SmartJs.Event.EventListener(onErrorProjectsHandler, this));
+    PocketCode.Proxy.send(srAllProjects);
+    // ---
+  } else {
+    //receivedObject = {"items":[
+    var item_arr = [];
+    for ( var key in server_known_errors)
+    {
+      // skip loop if the property is from prototype
+      if (!server_known_errors.hasOwnProperty(key)) continue;
+
+      var el = {};
+      el.id = key;
+      item_arr.push( el );
     }
-  };
+    receivedObject = {};
+    receivedObject.items = item_arr;
 
-  var onErrorProjectsHandler = function (e) {
-    console.log("---- ERROR ----");
-    console.log(e);
-    assert.ok(true, "Fetch of all projects failed");
-  };
+    console.log( receivedObject );
 
-  srAllProjects.onLoad.addEventListener(new SmartJs.Event.EventListener(onSuccessProjectsHandler, this));
-  srAllProjects.onError.addEventListener(new SmartJs.Event.EventListener(onErrorProjectsHandler, this));
-  PocketCode.Proxy.send(srAllProjects);
-  // ---
+    assert.ok(true, "only test " + limit + " projects from list.");
+    getSingleTestProject();
+  }
 
 
   // GAME ENGINE TESTS
@@ -131,35 +161,9 @@ QUnit.test("[missing]", function (assert) {
   };
 
 
-  /*
-  var gameEngineOnLoadError = function (e) {
-    //console.log("---- G:ERROR ----");
-    receivedResult = e;
-    var type = "";
-    if ((receivedResult instanceof Object)) {
-      type = "catched Error"; // receivedObject.target.keys()[0]; // e.g. ProjectNotFoundException
-    } else {
-      type = "Unknown target";
-    }
-
-    assert.ok(false, "Project ERROR " + id + " (" + type + ")");
-    done[currentProjectIdx - 1]();
-
-    // Free Memory
-    receivedResult = null;
-    e = null;
-    gameEngine.dispose();
-    gameEngine = new PocketCode.GameEngine();
-
-    getSingleTestProject();
-  };*/
-
   var gameEngine;
   var gameEngineOnLoadListener = new SmartJs.Event.EventListener(gameEngineOnLoad, this);
-  //var gameEngineOnLoadErrorListener = new SmartJs.Event.EventListener(gameEngineOnLoadError, this);
   // ---
-
-
 
   function startTimeOut() {
     timeout_timer = setTimeout(function(){ startNextTest() }, timeout_time);
@@ -193,32 +197,41 @@ QUnit.test("[missing]", function (assert) {
 
     var url = PocketCode.Services.PROJECT;
     id = receivedObject.items[currentProjectIdx-1].id;
+
     console.log( "start to test " + id + " (Nr." + currentProjectIdx + ")" );
     currentProjectIdx++;
 
-    /*
-    if( id == 881 || id == 1811 ) {
-      getSingleTestProject();
-      return;
-    } */
-    var sr = new PocketCode.ServiceRequest(url, SmartJs.RequestMethod.GET, {id: id});
+    if( test_only_listed_programs == false ) {
+      if (id in server_known_errors) {
+        assert.ok(true, "Project " + id + " skipped (" + server_known_errors[id] + ")");
+        done[currentProjectIdx - 1]();
+        getSingleTestProject();
+        return;
+      }
+    }
 
+    var sr = new PocketCode.ServiceRequest(url, SmartJs.RequestMethod.GET, {id: id});
     var onSuccessProjectHandler = function (e) {
 
       if (JsonToGameEngine == true) {
+
+        if (id in client_known_errors) {
+          assert.ok(true, "Project " + id + " skipped (" + client_known_errors[id] + ")");
+          done[currentProjectIdx - 1]();
+          getSingleTestProject();
+          return;
+        }
+
         var json = e.responseJson;
 
         // Test Loading Project Errors
         if (gameEngine) {
           gameEngine.onLoad.removeEventListener(gameEngineOnLoadListener);
-          //gameEngine.onLoadingError.removeEventListener(gameEngineOnLoadErrorListener);
           gameEngine = undefined;
         }
         gameEngine = new PocketCode.GameEngine();
         // define 2 EventListener
         gameEngine.onLoad.addEventListener(gameEngineOnLoadListener);
-        //gameEngine.onLoadingError.addEventListener(gameEngineOnLoadErrorListener);
-
 
         // load Project with json data
         try {
@@ -242,15 +255,12 @@ QUnit.test("[missing]", function (assert) {
           e = null;
           //gameEngine.dispose();
           getSingleTestProject();
-
         }
-
       } else {
         assert.ok(true, "Project " + id + " valid");
         done[currentProjectIdx - 1]();
         getSingleTestProject();
       }
-
     };
 
     var onErrorProjectHandler = function (e) {
@@ -261,18 +271,17 @@ QUnit.test("[missing]", function (assert) {
       } else {
         type = "Unknown Error (no Json Exception)";
       }
-
       assert.ok(false, "Project " + id + " not valid (" + type + ")");
       done[currentProjectIdx - 1]();
 
       getSingleTestProject();
 
     };
-
     sr.onLoad.addEventListener(new SmartJs.Event.EventListener(onSuccessProjectHandler, this));
     sr.onError.addEventListener(new SmartJs.Event.EventListener(onErrorProjectHandler, this));
     PocketCode.Proxy.send(sr);
   }
   // ---
+
 
 });
