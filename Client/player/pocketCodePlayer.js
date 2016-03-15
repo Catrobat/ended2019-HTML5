a054ccf2b58d94959400cd4b0821da006d4360cf
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2016-03-15 19:09:36 +0100

    change: no redirect on localhost
    - should improve local testing
    - if cross domain loading is not supported: redirect anyway

--- Client/player/pocketCodePlayer.js
+++ Client/player/pocketCodePlayer.js
@@ -852,8 +852,8 @@ PocketCode.Web = {
 		            window.setTimeout(this.launchProject.bind(this, projectId, rfc3066, containerElement), 100);
 		            return;
 		        }
-		        //if ((this._isMobile && window.location.href !== expectedUrl) || (PocketCode.crossOrigin.current && !PocketCode.crossOrigin.supported))
-		        //	window.location = expectedUrl;
+		        if ((this._isMobile && window.location.href !== expectedUrl && window.location.hostname !== 'localhost') || (PocketCode.crossOrigin.current && !PocketCode.crossOrigin.supported))
+		        	window.location = expectedUrl;
 		        if (this._isMobile) {
 		            this._launchMobile();
 		            return;
