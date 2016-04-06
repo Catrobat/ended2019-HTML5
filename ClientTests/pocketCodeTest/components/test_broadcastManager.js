12aa5e840a8f4e63a22455a72aa8ee44465e39fc
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2016-04-06 10:56:47 +0200

    broadcastMgr: extended tests

--- ClientTests/pocketCodeTest/components/test_broadcastManager.js
+++ ClientTests/pocketCodeTest/components/test_broadcastManager.js
@@ -66,12 +66,16 @@ QUnit.test("BroadcastManager: broadcast (simple)", function (assert) {
 
     //publish
     //broadcast
+    var l3 = new SmartJs.Event.EventListener(handler3, {_disposed: true});
+    b.subscribe("s12", l3);
     assert.throws(function () { b.publish(13); }, Error, "ERROR: broadcast: id invalid");
 
-    assert.ok(function () { b.publish("s12"); return true; }(), "broadcast: on empty subscription list");
+    assert.ok(function () { b.publish("s13"); return true; }(), "broadcast: on empty subscription list");
 
     b.subscribe("s12", l1);
     b.publish("s12");
+    assert.equal(b._subscriptions.s12.length, 2, "disposed subscribtion removed during publish");
+
     //assert.ok(handler1Called, "broadcast: single subscriber");
     //assert.equal(loopDelay, undefined, "loopDelay on empty subscription");
     //assert.equal(threadId, undefined, "threadId on empty subscription");
@@ -117,6 +121,7 @@ QUnit.test("BroadcastManager: broadcast (simple)", function (assert) {
 QUnit.test("BroadcastManager: broadcast & wait", function (assert) {
 
     var done1 = assert.async();
+    var done2 = assert.async();
 
     var handler1Called = false;
     var loopDelay, threadId;
@@ -129,10 +134,14 @@ QUnit.test("BroadcastManager: broadcast & wait", function (assert) {
         assert.ok(handler1Called, "broadcast wait: publisher feedback on empty subscription");
         assert.equal(loopDelay, undefined, "broadcast wait: loopDelay on empty subscription");
         assert.equal(threadId, "thread_id", "broadcast wait: threadId on empty subscription");
-
+        done1();    //make sure handler gets executed
     }
 
     var b = new PocketCode.BroadcastManager([{ id: "s12", name: "test" }]);
+    //stop
+    b._pendingBW = "other";
+    b.stop();
+    assert.deepEqual(b._pendingBW, {}, "stop: reset pending operations");
 
     //no subscribers
     b.publish("s12", new SmartJs.Event.EventListener(handler1, this), "thread_id");
@@ -143,7 +152,19 @@ QUnit.test("BroadcastManager: broadcast & wait", function (assert) {
     b.init([{ id: "s12", name: "test" }, { id: "s13", name: "test" }]);
 
     assert.throws(function () { b.publish("s12", new Function(), "asd"); }, Error, "ERROR: broadcast wait: publisher listener invalid");
+    assert.throws(function () { b.publish("s12", "listener"); }, Error, "ERROR: broadcast wait: invalid pubListener");
+    var l1 = new SmartJs.Event.EventListener(handler1, this);
     assert.throws(function () { b.publish("s12", l1); }, Error, "ERROR: broadcast wait: missing thread id");
+
+    //reinit
+    handler1Called = false;
+    loopDelay = undefined;
+    threadId = undefined;
+    handler1 = function (e) {
+        handler1Called = true;
+        loopDelay = e.loopDelay;
+        threadId = e.id;
+    }
 
     //reinit
     handler1Called = false;
@@ -181,6 +202,8 @@ QUnit.test("BroadcastManager: broadcast & wait", function (assert) {
     var brick2 = new TestBrick2("device", "sprite", b, { receiveMsgId: "s12" });
     var brick3 = new TestBrick2("device", "sprite", b, { receiveMsgId: "s12" });
     brick3.loopDelay = true;
+    var brick4 = new TestBrick2("device", "sprite", b, { receiveMsgId: "s12" });    //listener attached
+    brick4.dispose();   //brick/handler disposed?
 
     var startTime = new Date();
     var asyncHandler = function (e) {
@@ -192,11 +215,12 @@ QUnit.test("BroadcastManager: broadcast & wait", function (assert) {
         assert.equal(e.loopDelay, true, "broadcast wait: multiple subscribers: loopDelay");
         assert.equal(e.id, "thread_id", "broadcast wait: multiple subscribers: threadId");
 
-        done1();
+        done2();
         //startComplexTest1();
     };
 
     b.publish("s12", new SmartJs.Event.EventListener(asyncHandler, this), "thread_id");
+    assert.equal(b._subscriptions.s12.length, 3, "disposed subscribtion removed during publish");
 
 });
 
