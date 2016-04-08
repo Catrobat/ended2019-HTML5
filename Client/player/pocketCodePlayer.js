52c93f5672ada99271086dabe51d8b1fc69ede49
Wolfgang Wintersteller <wolfgang.wintersteller@gmail.com>
2016-04-08 20:01:11 +0200

    global error handling currently not in use
    handled by application after the framework was loaded

--- Client/player/pocketCodePlayer.js
+++ Client/player/pocketCodePlayer.js
@@ -673,23 +673,23 @@ PocketCode.Web = {
 
 		//methods
 		ResourceLoader.prototype = {
-			_onGlobalError: function (msg, url, line, col, error) {
-				// Note that col & error are new to the HTML 5 spec and may not be 
-				// supported in every browser.  It worked for me in Chrome.
-				var extra = !col ? '' : '\ncolumn: ' + col;
-				extra += !error ? '' : '\nerror: ' + error;
+			//_onGlobalError: function (msg, url, line, col, error) {
+			//	// Note that col & error are new to the HTML 5 spec and may not be 
+			//	// supported in every browser.  It worked for me in Chrome.
+			//	var extra = !col ? '' : '\ncolumn: ' + col;
+			//	extra += !error ? '' : '\nerror: ' + error;
 
-				// You can view the information in an alert to see things working like this:
-				alert("Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
+			//	// You can view the information in an alert to see things working like this:
+			//	alert("Error: " + msg + "\nurl: " + url + "\nline: " + line + extra);
 
-				// TODO: Report this error via ajax so you can keep track
-				//       of what pages have JS issues
+			//	// TODO: Report this error via ajax so you can keep track
+			//	//       of what pages have JS issues
 
-				var suppressErrorAlert = true;
-				// If you return true, then error alerts (like in older versions of 
-				// Internet Explorer) will be suppressed.
-				return suppressErrorAlert;
-			},
+			//	var suppressErrorAlert = true;
+			//	// If you return true, then error alerts (like in older versions of 
+			//	// Internet Explorer) will be suppressed.
+			//	return suppressErrorAlert;
+			//},
 			startLoading: function () {
 				this._loadingAborted = false;
 				var size = 0;
