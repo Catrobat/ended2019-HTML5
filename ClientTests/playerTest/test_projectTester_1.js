40b6e2dc9ca055b376ce36edb9a14ab71c10b7de
Michael Pittner <mpittner@student.tugraz.at>
2016-05-03 20:50:20 +0200

    add project-Test Class and 3 examples

---
+++ ClientTests/playerTest/test_projectTester_1.js
@@ -0,0 +1,48 @@
+'use strict';
+
+QUnit.module("Test 10 RANDOM projects - FULL");
+
+QUnit.test("projectTesterClass", function (assert) {
+
+
+  /* ********************************** */
+  /* Example to test a list of Projects */
+  /* ********************************** */
+
+
+
+  // Create Test Instance
+  var test = new PocketCode.ProjectTesterClass();
+  var last_assert = assert.async();
+
+
+  var singleProjectHandler = function (e) {
+    assert.ok( true, e.print );
+    if( test._nextTest() == "last_call" )
+      last_assert();
+  };
+
+  var errorHandler = function (e) {
+    assert.ok( false, e.print );
+    if( test._nextTest() == "last_call" )
+      last_assert();
+  };
+
+  // define own settings
+  var settings = {
+    method: test._methods.FULL,
+    limit: 10,
+    mask: test._mask.RANDOM
+  };
+
+  // Add Listener
+  test.onGetProject.addEventListener(new SmartJs.Event.EventListener(singleProjectHandler, this));
+  test.onGetError.addEventListener(new SmartJs.Event.EventListener(errorHandler, this));
+
+  // Fetch Projects
+  test._fetchProjects( settings );
+
+  // start test
+  test._startTests();
+
+});
---
+++ ClientTests/playerTest/test_projectTester_3.js
@@ -0,0 +1,43 @@
+'use strict';
+
+QUnit.module("Test ALL projects sorted by views - FULL");
+
+QUnit.test("projectTesterClass", function (assert) {
+
+
+  /* ********************************** */
+  /* Example to test a list of Projects */
+  /* ********************************** */
+
+  // Create Test Instance
+  var test = new PocketCode.ProjectTesterClass();
+  var last_assert = assert.async();
+
+
+  var singleProjectHandler = function (e) {
+    assert.ok( true, e.print );
+    if( test._nextTest() == "last_call" )
+      last_assert();
+  };
+
+  var errorHandler = function (e) {
+    assert.ok( false, e.print );
+    if( test._nextTest() == "last_call" )
+      last_assert();
+  };
+
+  // define own settings
+  var settings = {
+    method: test._methods.FULL,
+    mask: test._mask.VIEWS,
+    limit: test._limits.ALL
+  };
+
+  // Add Listener
+  test.onGetProject.addEventListener(new SmartJs.Event.EventListener(singleProjectHandler, this));
+  test.onGetError.addEventListener(new SmartJs.Event.EventListener(errorHandler, this));
+
+  // start test
+  test._startTests( settings );
+
+});
