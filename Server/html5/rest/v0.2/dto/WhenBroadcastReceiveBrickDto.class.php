998b6848760bce71a907bfd39d2db3ca90969b4d
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2016-05-19 21:24:53 +0200

    HTML-64 Brick Refactoring
    Changed/renamed bricks (naming conventions for extensions)

--- Server/html5/rest/v0.2/dto/WhenBroadcastReceiveBrickDto.class.php
+++ Server/html5/rest/v0.2/dto/WhenBroadcastReceiveBrickDto.class.php
@@ -1,13 +1,11 @@
 <?php
 
-class BroadcastReceiveBrickDto extends BaseBrickDto {
+class WhenBroadcastReceiveBrickDto extends ScriptBlockDto {
 
   public $receiveMsgId;	//id of projects broadcasts
-  public $bricks = array();	//inner scripts
-  
   
   public function __construct($receiveMsgId) {
-	parent::__construct("BroadcastReceive");
+	parent::__construct("WhenBroadcastReceive");
     
     $this->receiveMsgId = $receiveMsgId;
   }
