250a1fd8ab1ea98491fdf8d6da385d4a6bb4e891
Benny Bristow <benny6890@gmail.com>
2016-07-02 11:56:39 +0200

    HTML-63 Tests for collisionManager.subscribe

--- ClientTests/pocketCodeTest/components/test_collisionManager.js
+++ ClientTests/pocketCodeTest/components/test_collisionManager.js
@@ -10,6 +10,29 @@ QUnit.test("CollisionManager", function (assert) {
     
     var cm = new PocketCode.CollisionManager();
 
-    assert.ok(false, "TODO");
+    cm._sprites = [
+        {id: "s1"},
+        {id: "s2"},
+        {id: "s3"},
+        {id: "s4"}
+    ];
+
+    cm.subscribe(cm._sprites[2], cm._sprites[0]);
+    assert.ok(cm._registeredCollisions[cm._sprites[2].id], "added subscribed sprite to registered sprites");
+    assert.ok(cm._registeredCollisions[cm._sprites[2].id].indexOf(cm._sprites[0].id) >= 0, "added subscribed sprite to registered sprites");
+
+    cm.subscribe(cm._sprites[0], cm._sprites[2]);
+    assert.ok(!cm._registeredCollisions[cm._sprites[0].id], "duplicate subscription not added");
+
+    cm.subscribe(cm._sprites[0], cm._sprites[1]);
+    assert.ok(cm._registeredCollisions[cm._sprites[0].id], "added non duplicate subscribed sprite to registered sprites");
+    assert.ok(cm._registeredCollisions[cm._sprites[0].id].indexOf(cm._sprites[1].id) >= 0, "added subscribed sprite to registered sprites");
+
+    cm.subscribe(cm._sprites[0], cm._sprites[3]);
+    assert.equal(cm._registeredCollisions[cm._sprites[0].id].length, 2, "further collision added to the correct sprite");
+
+    cm.unsubscribe(cm._sprites[0], cm._sprites[2]);
+    assert.ok(!cm._registeredCollisions[cm._sprites[2]], "Sprite with no registered collisions removed from registered collisions");
+    assert.equal(cm._registeredCollisions[cm._sprites[0].id].length, 2, "collisions that are still needed are not removed");
 
 });
