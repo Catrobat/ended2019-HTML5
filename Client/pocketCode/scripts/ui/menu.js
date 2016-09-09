942225a3237a27a1f517a6c00c4f5752bf0f8f86
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2016-09-09 14:40:47 +0200

    HTML-65 Review and cleanup
    getting rid of some containers, cleanup code and interfaces

--- Client/pocketCode/scripts/ui/menu.js
+++ Client/pocketCode/scripts/ui/menu.js
@@ -7,180 +7,135 @@
 'use strict';
 
 PocketCode.Ui.Menu = (function () {
-  Menu.extends(SmartJs.Ui.ContainerControl, false);
+    Menu.extends(SmartJs.Ui.ContainerControl, false);
 
-  //cntr
-  function Menu(args) {
-    SmartJs.Ui.ContainerControl.call(this, { className: 'pc-topElement' });
+    //cntr
+    function Menu(args) {
+        SmartJs.Ui.ContainerControl.call(this, { className: 'pc-menu' });
 
-    this._states = {
-      CLOSED: 'open',
-      OPEN: 'closed'
-    };
+        if (SmartJs.Device.isMobile)
+            this.addClassName('pc-menuMobile');
 
-    // Object parameters
-    this._state = this._states.OPEN;
-    this._minHeight = 200;
-    this._dom.disabled = false;
+        this._menuButton = new SmartJs.Ui.ContainerControl({ className: 'pc-menuButton' });
+        this.appendChild(this._menuButton);
 
+        var btnControl = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.MENU, undefined, false, true);
+        btnControl.style.margin = '2px';
+        btnControl.onClick.addEventListener(new SmartJs.Event.EventListener(this._openCloseHandler, this));
+        this._menuButton.appendChild(btnControl);
+        this._subMenu = new SmartJs.Ui.ContainerControl({ className: 'pc-subMenu' });
+        this._subMenu.hide();   //default
+        this._appendChild(this._subMenu);
+        this._container = new PocketCode.Ui.ScrollContainer();
+        this._subMenu.appendChild(this._container);
 
-    //var img_tag = new SmartJs.Ui.Image({ style: { width: '100%' } });
-    var img_tag = new PocketCode.Ui.PlayerSvgButton(PocketCode.Ui.SvgImageString.MENU, null, false, true);
-    img_tag.className += ' pc-menuSymbol';
+        //events
+        this._onMenuAction = new SmartJs.Event.Event(this);
+        this._onOpen = new SmartJs.Event.Event(this);
 
-    var topContainer = new SmartJs.Ui.ContainerControl({ className: 'pc-menuAlign' });
-    var wholeMenuContainer = new SmartJs.Ui.ContainerControl({ className: 'pc-mainMenu' });
-    var innerMenuContainer = new SmartJs.Ui.ContainerControl({ className: 'pc-menuContainer' });
-    var titleAndContentContainer = new SmartJs.Ui.ContainerControl({ className: 'pc-menu' });
-    this._menuTitle = new SmartJs.Ui.ContainerControl({ className: 'pc-menuTitle' });
-    this._menuTitle.appendChild( img_tag );
-    this._menuLabel = img_tag;
-    var contentContainer = new SmartJs.Ui.ContainerControl({ className: 'pc-submenu' });
-    var scrollContainer = new SmartJs.Ui.ContainerControl({ className: 'pc-scrollContainer' });
-    this._container = new PocketCode.Ui.ScrollContainer();
-    this._container.style.padding = 0;
-
-    scrollContainer.appendChild( this._container );
-    contentContainer.appendChild( scrollContainer );
-    titleAndContentContainer.appendChild( this._menuTitle );
-    titleAndContentContainer.appendChild( contentContainer );
-    innerMenuContainer.appendChild( titleAndContentContainer );
-    wholeMenuContainer.appendChild( innerMenuContainer );
-    topContainer.appendChild( wholeMenuContainer );
-    this._appendChild( topContainer );
+        PocketCode.I18nProvider.onDirectionChange.addEventListener(new SmartJs.Event.EventListener(function (e) { this._dom.dir = e.direction; }, this));
+    }
 
     //events
-    this._onClick = new SmartJs.Event.Event(this);
-    //this._menuTitle._dom.onClick.addEventListener(new SmartJs.Event.EventListener(function (e) { this._clickHandler.dispatchEvent(); }, this));
+    Object.defineProperties(Menu.prototype, {
+        onMenuAction: {
+            get: function () {
+                return this._onMenuAction;
+            }
+        },
+        onOpen: {
+            get: function () {
+                return this._onOpen;
+            }
+        },
+    });
 
-    this._addDomListener(this._menuTitle._dom, 'click', this._clickHandler);
-    this._addDomListener(this._menuTitle._dom, 'touchstart', function (e) { }, { cancelBubble: true, stopPropagation: false, systemAllowed: true });   //allow system events to show css (pressed) on buttons
-    this._onResize.addEventListener(new SmartJs.Event.EventListener(this._resizeHandler, this));
-    //window.addEventListener('resize', alert());
-  }
+    //methods
+    Menu.prototype.merge({
+        _openCloseHandler: function (e) {
+            if (this._subMenu.hidden) {
+                this._subMenu.show();
+                this._menuButton.addClassName('pc-menuButtonOpened');
+                this._onOpen.dispatchEvent();
+            } else {
+                this.close();
+            }
+            this._dom.blur();   //???
+        },
+        close: function (e) {
+            this._subMenu.hide();
+            this._menuButton.removeClassName('pc-menuButtonOpened');
+        },
+        addElement: function (element) {
+            console.log("add element");
+            this._container.appendChild(element)
+            console.log(this._container);
+            this._container.onResize.dispatchEvent();
+        },
+    });
 
-  //properties
-  Object.defineProperties(Menu.prototype, {
-    disabled: {
-      get: function () {
-        return this._dom.disabled;
-      },
-      set: function (value) {
-        this._dom.disabled = value;
-      }
-    }
-  });
-
-  //events
-  Object.defineProperties(Menu.prototype, {
-    onClick: {
-      get: function () {
-        return this._onClick;
-      }
-    }
-  });
-
-  //methods
-  Menu.prototype.merge({
-    _clickHandler: function (e) {
-      if( this._state == this._states.CLOSED ) {
-        this._open();
-      } else {
-        this._close();
-      }
-      this._dom.blur();
-      this._onClick.dispatchEvent();
-    },
-    _open: function(e) {
-      this._container.style.display = 'block';
-      this._state = this._states.OPEN;
-      this._onResize.dispatchEvent();
-    },
-    _close: function(e) {
-      this._container.style.display = 'none';
-      this._state = this._states.CLOSED;
-      this._onResize.dispatchEvent();
-    },
-    addElement: function(element) {
-      console.log( "add element" );
-      this._container.appendChild( element )
-      console.log( this._container );
-      this._container.onResize.dispatchEvent();
-    },
-    _resizeHandler: function (e) {
-      console.log('resize...');
-      //console.log( this._menuLabel.height );
-      var availableHeight = document.documentElement.clientHeight - this._menuLabel.height - 20;
-      var minHeight = this._minHeight - (this._menuLabel.height);
-
-      //console.log(availableHeight);
-      if (availableHeight > minHeight)
-        this._container.style.maxHeight = availableHeight + 'px';
-      else
-        this._container.style.maxHeight = minHeight + 'px';
-      }
-  });
-
-  return Menu;
+    return Menu;
 })();
 
 PocketCode.Ui.MenuSeparator = (function () {
-  MenuSeparator.extends(SmartJs.Ui.Control, false);
+    MenuSeparator.extends(SmartJs.Ui.Control, false);
 
-  //cntr
-  function MenuSeparator(args) {
-    SmartJs.Ui.Control.call(this, 'div', { className: 'pc-menuItemSep' });
-    this._dom.appendChild(document.createElement('hr'));
-  }
+    //cntr
+    function MenuSeparator(args) {
+        SmartJs.Ui.Control.call(this, 'div', { className: 'pc-menuSeparator' });
+        this._dom.appendChild(document.createElement('hr'));
+    }
 
-  return MenuSeparator;
+    return MenuSeparator;
 })();
 
 PocketCode.Ui.MenuItem = (function () {
-  MenuItem.extends(PocketCode.Ui.Button, false);
+    MenuItem.extends(PocketCode.Ui.Button, false);
 
-  //cntr
-  function MenuItem(i18nKey) {
-    PocketCode.Ui.Button.call(this, i18nKey, {className: 'pc-menuItem'});
-  }
+    //cntr
+    function MenuItem(i18nKey) {
+        PocketCode.Ui.Button.call(this, i18nKey, { className: 'pc-menuItem' });
+    }
 
 
-  return MenuItem;
+    return MenuItem;
 })();
 
+/*
 PocketCode.Ui.SubMenu = (function () {
-  SubMenu.extends(SmartJs.Ui.ContainerControl, false);
+    SubMenu.extends(SmartJs.Ui.ContainerControl, false);
 
 
-  //cntr
-  function SubMenu() {
-    SmartJs.Ui.ContainerControl.call(this, { className: 'pc-topElement2' });
+    //cntr
+    function SubMenu() {
+        SmartJs.Ui.ContainerControl.call(this, { className: 'pc-topElement2' });
 
-    this._states = {
-      CLOSED: 'open',
-      OPEN: 'closed'
-    };
-
-
-
-    // Object parameters
-    this._state = this._states.OPEN;
-    this._content = null;
-    this._minHeight = 200;
+        this._states = {
+            CLOSED: 'open',
+            OPENED: 'closed'
+        };
 
 
-    var sb1 = new SmartJs.Ui.ContainerControl({ className: 'pc-submenu' });
 
-    var button7 = new PocketCode.Ui.MenuItem('hey');
-    var button8 = new PocketCode.Ui.MenuItem('submenu');
-    sb1.appendChild( button7 );
-    var section = new SmartJs.Ui.ContainerControl();
-    section.appendChild( button8 );
-    sb1.appendChild( section );
-    this._dom.appendChild( sb1._dom );
-    //this._dom = test;
+        // Object parameters
+        this._state = this._states.CLOSED;
+        this._content = null;
+        this._minHeight = 200;
 
-  }
 
-  return SubMenu;
+        var sb1 = new SmartJs.Ui.ContainerControl({ className: 'pc-subMenu' });
+
+        var button7 = new PocketCode.Ui.MenuItem('hey');
+        var button8 = new PocketCode.Ui.MenuItem('submenu');
+        sb1.appendChild(button7);
+        var section = new SmartJs.Ui.ContainerControl();
+        section.appendChild(button8);
+        sb1.appendChild(section);
+        this._dom.appendChild(sb1._dom);
+        //this._dom = test;
+
+    }
+
+    return SubMenu;
 })();
+*/
