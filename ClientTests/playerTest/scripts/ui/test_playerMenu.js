4dc37a65af3ba3f7dbc4a038fabca2f0daa5cf55
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2016-09-10 11:56:08 +0200

    TestFramework for Player Scripts
    - fixed references and added ctr calls to make sure all references are
    loaded correctly
    - added testfile for playerMenu

---
+++ ClientTests/playerTest/scripts/ui/test_playerMenu.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../qunit/qunit-1.23.0.js" />
+/// <reference path="../../../../Client/pocketCode/scripts/ui/menu.js" />
+/// <reference path="../../../../Client/player/scripts/ui/playerMenu.js" />
+'use strict';
+
+QUnit.module("scripts/ui/playerMenu.js");
+
+
+QUnit.test("Menu", function (assert) {
+
+    var ctrl = new PocketCode.Player.Ui.Menu();
+    assert.ok(false, "TODO");
+});
+
--- ClientTests/playerTest/test_pocketCodePlayer.js
+++ ClientTests/playerTest/test_pocketCodePlayer.js
@@ -1,12 +1,48 @@
 ﻿/// <reference path="../qunit/qunit-1.23.0.js" />
-/// <reference path="../../Client/player/pocketCodePlayer.js" />
+/// <reference path="../../Client/player/pocketCodeWeb.js" />
 'use strict';
 
-QUnit.module("player/pocketCodePlayer.js");
+QUnit.module("player/pocketCodeWeb.js");
+//IMPORTANT: most of the code is executed before the framework (all scripts) are loaded: make sure to adapt the tests to use default javascript only
 
+QUnit.test("PocketCodePlayer: global properties", function (assert) {
 
-QUnit.test("PocketCodePlayer", function (assert) {
+    assert.ok(false, "TODO");
+});
 
+QUnit.test("PocketCodePlayer: PocketCode.Web.FullscreenApi", function (assert) {
+
+    var ctrl = PocketCode.Web.FullscreenApi;    //not a construktor: static
+    assert.ok(false, "TODO");
+});
+
+QUnit.test("PocketCodePlayer: PocketCode.Web.ExitButton", function (assert) {
+
+    var ctrl = new PocketCode.Web.ExitButton();
+    assert.ok(false, "TODO");
+});
+
+QUnit.test("PocketCodePlayer: PocketCode.Web.WebOverlay", function (assert) {
+
+    var ctrl = new PocketCode.Web.WebOverlay();
+    assert.ok(false, "TODO");
+});
+
+QUnit.test("PocketCodePlayer: PocketCode.Web.LoadingIndicator", function (assert) {
+
+    var ctrl = new PocketCode.Web.LoadingIndicator();
+    assert.ok(false, "TODO");
+});
+
+QUnit.test("PocketCodePlayer: PocketCode.Web.SplashScreen", function (assert) {
+
+    var ctrl = new PocketCode.Web.SplashScreen();
+    assert.ok(false, "TODO");
+});
+
+QUnit.test("PocketCodePlayer: PocketCode.Web.ResourceLoader", function (assert) {
+
+    var ctrl = new PocketCode.Web.ResourceLoader(PocketCode.Web.resources);
     assert.ok(false, "TODO");
 });
 
