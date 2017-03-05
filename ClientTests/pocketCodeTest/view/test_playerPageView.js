5391fcaaade192fd6831d6afebd9b5ed498f6e76
anja11 <anja.reibenbacher@student.tugraz.at>
2017-03-05 15:49:01 +0100

    HTML-89 test_playerPageView.js

--- ClientTests/pocketCodeTest/view/test_playerPageView.js
+++ ClientTests/pocketCodeTest/view/test_playerPageView.js
@@ -11,8 +11,111 @@ QUnit.module("view/playerPageView.js");
 
 QUnit.test("PlayerPageView", function (assert) {
 
-    assert.ok(false, "TODO");
+    //var done1 = assert.async();
+
+    var PlayerPageView = new PocketCode.Ui.PlayerPageView();
+    assert.ok(PlayerPageView instanceof SmartJs.Ui.ContainerControl, "instance check");
+    assert.ok(PlayerPageView._footer.hidden == true, "footer hidden");
+    assert.ok(PlayerPageView._header.hidden == true, "header hidden");
+    assert.ok(PlayerPageView.__container._childs[0] instanceof PocketCode.Ui.PlayerToolbar &&
+        PlayerPageView.__container._childs[1] instanceof PocketCode.Ui.PlayerStartScreen, "add toolbar and startScreen to childs");
+    assert.ok(PlayerPageView._onExit instanceof SmartJs.Event.Event, "add onExit Event");
+    assert.ok(PlayerPageView._startScreen.hidden == true, "startScreen hidden");
+
+    // ********************* properties *********************
+
+    //menu
+    var menu = PlayerPageView.menu;
+    assert.equal(menu,  PlayerPageView._menu, "menu getter");
+
+    //executionState
+    PlayerPageView.executionState = 1;
+    assert.ok(PlayerPageView._toolbar._startButton.hidden == true &&
+        PlayerPageView._toolbar._pauseButton.hidden == false &&
+        PlayerPageView._toolbar._executionState == 1, "set executionstate");
+
+    var executionState = PlayerPageView.executionState;
+    assert.equal(PlayerPageView._toolbar.executionState, executionState, "get executionstate");
+
+    //disabled
+    assert.ok(PlayerPageView._toolbar._backButton.disabled == true &&
+        PlayerPageView._toolbar._restartButton.disabled == true &&
+        PlayerPageView._toolbar._screenshotButton.disabled == true &&
+        PlayerPageView._toolbar._axesButton.disabled == true &&
+        PlayerPageView._startScreen._startButton.disabled == true, "disabled");
+
+    PlayerPageView.disabled = false;
+    assert.ok(PlayerPageView._toolbar._backButton.disabled == false &&
+        PlayerPageView._toolbar._restartButton.disabled == false &&
+        PlayerPageView._startScreen._startButton.disabled == false, "disabled 2");
+
+    //axesButtonChecked
+    PlayerPageView.axesButtonChecked = true;
+    assert.ok(PlayerPageView._toolbar._axesButton.checked == true, "axesButtonChecked");
+    PlayerPageView.axesButtonChecked = false;
+    assert.ok(PlayerPageView._toolbar._axesButton.checked == false, "axesButtonChecked 2");
+
+    //axesButtonDisabled
+    PlayerPageView.axesButtonDisabled = false;
+    assert.ok(PlayerPageView._toolbar._axesButtonDisabled == false &&
+        PlayerPageView._toolbar._axesButton.disabled == false, "axesButtonDisabled");
+    PlayerPageView.axesButtonDisabled = true;
+    assert.ok(PlayerPageView._toolbar._axesButtonDisabled == true &&
+        PlayerPageView._toolbar._axesButton.disabled == true, "axesButtonDisabled 2");
+
+    //backButtonDisabled
+    PlayerPageView.backButtonDisabled = true;
+    assert.ok(PlayerPageView._toolbar._backButtonDisabled == true &&
+        PlayerPageView._toolbar._backButton.disabled == true, "backButtonDisabled");
+    PlayerPageView.backButtonDisabled = false;
+    assert.ok(PlayerPageView._toolbar._backButtonDisabled == false &&
+        PlayerPageView._toolbar._backButton.disabled == false, "backButtonDisabled 2");
+
+    //screenshotButtonDisabled
+    PlayerPageView.screenshotButtonDisabled = false;
+    assert.ok(PlayerPageView._toolbar._screenshotButtonDisabled == false &&
+        PlayerPageView._toolbar._screenshotButton.disabled == false, "screenshotButtonDisabled");
+    PlayerPageView.screenshotButtonDisabled = true;
+    assert.ok(PlayerPageView._toolbar._screenshotButtonDisabled == true &&
+        PlayerPageView._toolbar._screenshotButton.disabled == true, "screenshotButtonDisabled 2");
+
+    // ********************* events *********************
+
+    /*function onToolbarButtonClicked(){
+        assert(true, "onToolbarButtonClicked Event dispatched");
+        done1();
+    }
+    PlayerPageView.onToolbarButtonClicked.addEventListener(new SmartJs.Event.EventListener(onToolbarButtonClicked));*/
+
+    assert.ok(PlayerPageView.onToolbarButtonClicked instanceof SmartJs.Event.Event, "onToolbarButtonClicked getter");
+    assert.ok(PlayerPageView.onMenuAction instanceof SmartJs.Event.Event, "onMenuAction getter");
+    assert.ok(PlayerPageView.onMenuOpen instanceof SmartJs.Event.Event, "onMenuOpen getter");
+    assert.ok(PlayerPageView.onStartClicked instanceof SmartJs.Event.Event, "onStartClicked getter");
+    assert.ok(PlayerPageView.onExitClicked instanceof SmartJs.Event.Event, "onExitClicked getter");
+
+    // ********************* methods *********************
+
+    //showStartScreen
+    PlayerPageView.showStartScreen("title1", "null");
+    assert.ok(PlayerPageView._startScreen.title == "title1" &&
+        PlayerPageView._startScreen._previewImage == "https://share.catrob.at//images/default/screenshot.png", "title and previewImage set");
+    assert.ok(PlayerPageView._startScreen._dom.style.display == "", "display == ''");
+    PlayerPageView.showStartScreen("title2", "url");
+    assert.ok(PlayerPageView._startScreen.title == "title2" &&
+        PlayerPageView._startScreen._previewImage == "url", "title and previewImage set 2");
+
+    //hideStartScreen
+    PlayerPageView.hideStartScreen();
+    assert.ok(PlayerPageView._startScreen.hidden == true, "startScreen hidden");
+    assert.ok(PlayerPageView._startScreen._dom.style.display == "none", "display == 'none'");
+    assert.ok(PlayerPageView._header.hidden == true, "header hidden");
+
+    //setLoadingProgress
+    PlayerPageView.setLoadingProgress(10.0);
+    assert.ok(PlayerPageView._startScreen._progressBar.progressBar.style.width == "10%", "setLoadingProgress: width set to 10%");
+
+    //closeMenu
+    PlayerPageView.closeMenu();
+    assert.ok(PlayerPageView._menu._subMenu._dom.style.display == "none", "close menu, submenu display: none");
+    //assert.ok(PlayerPageView._menu._menuButton._dom.className == "", "classname removed");
 });
-
-
-
