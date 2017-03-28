4a08f46c338d27737e9bb18c9e8b34d2521658da
anja11 <anja.reibenbacher@student.tugraz.at>
2017-03-28 17:10:23 +0200

    HTML-163,  codeSceneController/View

---
+++ Client/codeview/scripts/controller/CodeSceneController.js
@@ -0,0 +1,19 @@
+
+
+PocketCode.CodeSceneController = (function () {
+    CodeSceneController.extends(PocketCode.PageController, false);
+
+    function CodeSceneController() {
+
+    }
+
+    //properties
+    Object.defineProperties(CodeSceneController.prototype, {
+    });
+
+    //methods
+    CodeSceneController.prototype.merge({
+    });
+
+    return CodeSceneController;
+})();
