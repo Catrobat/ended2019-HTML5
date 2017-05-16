2b58dbfee9e6928323a32327cb8fed41f5de9713
Dino Keskic <dino.keskic@student.tugraz.at>
2017-05-16 09:53:27 +0200

    disabled mobile reloading

--- Client/player/pocketCodePlayer.js
+++ Client/player/pocketCodePlayer.js
@@ -6,7 +6,7 @@
 if (!PocketCode)
     var PocketCode = {};
 
-PocketCode.Local = 0;
+PocketCode.Local = 1;
 
 
 if (PocketCode.Local === 1)
@@ -887,8 +887,8 @@ PocketCode.Web = {
                     window.setTimeout(this.launchProject.bind(this, projectId, rfc3066, containerElement), 100);
                     return;
                 }
-                if ((this._isMobile && window.location.href !== expectedUrl && window.location.hostname !== 'localhost') || (PocketCode.crossOrigin.current && !PocketCode.crossOrigin.supported))
-                    window.location = expectedUrl;
+                //if ((this._isMobile && window.location.href !== expectedUrl && window.location.hostname !== 'localhost') || (PocketCode.crossOrigin.current && !PocketCode.crossOrigin.supported))
+                   // window.location = expectedUrl;
                 if (this._isMobile) {
                     this._launchMobile();
                     return;
