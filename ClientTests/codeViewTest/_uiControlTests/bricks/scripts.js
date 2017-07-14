c22d92e4b92667d362a5af97954cde01d99a569c
Anja Reibenbacher <anja.reibenbacher@student.tugraz.at>
2017-07-14 14:11:31 +0200

    codeView li without bullet point

--- ClientTests/codeViewTest/_uiControlTests/bricks/scripts.js
+++ ClientTests/codeViewTest/_uiControlTests/bricks/scripts.js
@@ -7,9 +7,6 @@ window.onload = function () {
 
 
     var device = "device";
-    /*var gameEngine = new PocketCode.GameEngine();
-    var scene = new PocketCode.Model.Scene(gameEngine, undefined, undefined, []);
-    var sprite = new PocketCode.Model.Sprite(gameEngine, scene, { id: "spriteId", name: "spriteName" });*/
     var duration = JSON.parse('{"type":"NUMBER","value":"0.5","right":null,"left":null}');
     var b = new PocketCode.Model.WaitBrick("device", "sprite", { duration: duration });
 
