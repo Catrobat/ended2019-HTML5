2fca20c55ba3091aed1a9d843e4ad28a2ee5cb25
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2017-07-25 21:59:11 +0200

    tab control: merged

--- Client/pocketCode/scripts/ui/tab.js
+++ Client/pocketCode/scripts/ui/tab.js
@@ -1,4 +1,4 @@
-/// <reference path="../../../smartJs/sj.js" />
+﻿/// <reference path="../../../smartJs/sj.js" />
 /// <reference path="../../../smartJs/sj-core.js" />
 /// <reference path="../../../smartJs/sj-event.js" />
 /// <reference path="../../../smartJs/sj-ui.js" />
@@ -6,65 +6,159 @@
 /// <reference path="../ui.js" />
 'use strict';
 
-PocketCode.Ui.merge({
-    TabControl: (function () {
+PocketCode.Ui = {
+    TabControl: (function () {  //caption, showCaption, addTabs[], removeTab, activeTab: get/set id
         TabControl.extends(SmartJs.Ui.ContainerControl, false);
 
         //cntr
-        function TabControl(i18nKey) {
-            SmartJs.Ui.ContainerControl.call(this, { className: 'pc-menuItem' });
+        function TabControl(i18nKey, args) {
+            SmartJs.Ui.ContainerControl.call(this, args);
 
+            this._textNode = new PocketCode.Ui.I18nTextNode(i18nKey);
+            this._appendChild(this._textNode);
+
+            //events
+            this._onClick = new SmartJs.Event.Event(this);
+            this._addDomListener(this._dom, 'click', this._clickHandler);
+            this._btnListener = this._addDomListener(this._dom, 'touchstart', function (e) { }, { cancelBubble: true, stopPropagation: false, systemAllowed: true });   //allow system events to show css (pressed) on TabControls
+            //this._addDomListener(this._dom, 'touchend', this._clickHandler, { cancelBubble: true });//function (e) { this._dom.click(); });
         }
 
+        ////events
+        //Object.defineProperties(TabControl.prototype, {
+        //    onClick: {
+        //        get: function () {
+        //            return this._onClick;
+        //        },
+        //    },
+        //});
+
+        //properties
+        Object.defineProperties(TabControl.prototype, {
+            text: {
+                get: function () {
+                    return this._textNode.text;
+                },
+                //set: function (value) {
+                //    this._textNode.text = value;
+                //},
+            },
+            i18nKey: {
+                set: function (i18n) {
+                    this._textNode.i18n = i18n;
+                },
+            },
+            disabled: {
+                get: function () {
+                    return this._dom.disabled;
+                },
+                set: function (value) {
+                    this._dom.disabled = value;
+                },
+            },
+        });
+
+        //methods
         TabControl.prototype.merge({
+            _clickHandler: function (e) {
+                this._dom.blur();
+                this._onClick.dispatchEvent();
+            },
             /* override */
-            appendChild: function (uiControl) {
-                if (!(uiControl instanceof PocketCode.Ui.TabPage))
+            appendChild: function (tabPage) {
+                if (!(tabPage instanceof PocketCode.Ui.TabPage))
                     throw new Error('invalid argument: tab page');
-                SmartJs.Ui.ContainerControl.prototype.appendChild.call(this, uiControl);
+                return SmartJs.Ui.ContainerControl.prototype.appendChild.call(tabPage);
             },
-            insertAt: function (idx, uiControl) {
-                if (!(uiControl instanceof PocketCode.Ui.TabPage))
+            insertAt: function (idx, tabPage) {
+                if (!(tabPage instanceof PocketCode.Ui.TabPage))
                     throw new Error('invalid argument: tab page');
-                SmartJs.Ui.ContainerControl.prototype.insertAt.call(this, idx, uiControl);
+                return SmartJs.Ui.ContainerControl.prototype.insertAt.call(idx, tabPage);
             },
-            insertBefore: function (newUiC, existingUiC) {
-                if (!(newUiC instanceof PocketCode.Ui.TabPage) || !(existingUiC instanceof PocketCode.Ui.TabPage))
+            insertBefore: function (newTabPage, existingTabPage) {
+                if (!(newTabPage instanceof PocketCode.Ui.TabPage) || !(existingTabPage instanceof PocketCode.Ui.TabPage))
                     throw new Error('invalid argument: tab page');
-                SmartJs.Ui.ContainerControl.prototype.insertBefore.call(this, newUiC, existingUiC);
+                return SmartJs.Ui.ContainerControl.prototype.insertBefore.call(newTabPage, existingTabPage);
             },
-            insertAfter: function (newUiC, existingUiC) {
-                if (!(newUiC instanceof PocketCode.Ui.TabPage) || !(existingUiC instanceof PocketCode.Ui.TabPage))
+            insertAfter: function (newTabPage, existingTabPage) {
+                if (!(newTabPage instanceof PocketCode.Ui.TabPage) || !(existingTabPage instanceof PocketCode.Ui.TabPage))
                     throw new Error('invalid argument: tab page');
-                SmartJs.Ui.ContainerControl.prototype.insertAfter.call(this, newUiC, existingUiC);
+                return SmartJs.Ui.ContainerControl.prototype.insertAfter.call(newTabPage, existingTabPage);
             },
-            replaceChild: function (newUiC, existingUiC) {
-                if (!(newUiC instanceof PocketCode.Ui.TabPage) || !(existingUiC instanceof PocketCode.Ui.TabPage))
+            replaceChild: function (newTabPage, existingTabPage) {
+                if (!(newTabPage instanceof PocketCode.Ui.TabPage) || !(existingTabPage instanceof PocketCode.Ui.TabPage))
                     throw new Error('invalid argument: tab page');
-                SmartJs.Ui.ContainerControl.prototype.replaceChild.call(this, newUiC, existingUiC);
+                return SmartJs.Ui.ContainerControl.prototype.replaceChild.call(newTabPage, existingTabPage);
             },
-            removeChild: function (uiControl) {
-                if (!(uiControl instanceof PocketCode.Ui.TabPage))
-                    throw new Error('invalid argument: tab');
-                SmartJs.Ui.ContainerControl.prototype.appendChild.call(this, uiControl);
+            removeChild: function (tabPage) {
+                if (!(tabPage instanceof PocketCode.Ui.TabPage))
+                    throw new Error('invalid argument: tab page');
+                return SmartJs.Ui.ContainerControl.prototype.removeChild.call(tabPage);
             },
         });
 
         return TabControl;
     })(),
 
-    TabPage: (function () {
+    TabPage: (function () { //id, caption, body
         TabPage.extends(SmartJs.Ui.ContainerControl, false);
 
         //cntr
-        function TabPage(i18nKey) {
-            SmartJs.Ui.ContainerControl.call(this, { className: 'pc-menuItem' });
+        function TabPage(i18nKey, args) {
+            SmartJs.Ui.ContainerControl.call(this, args);
 
-            //this._removeDomListener(this._dom, 'touchstart', this._btnListener);    //make sure events for scrolling get passed
-            //this._addDomListener(this._dom, 'touchstart', function (e) { }, { stopPropagation: false, systemAllowed: true });   //allow system events to show css (pressed) on buttons
+            this._textNode = new PocketCode.Ui.I18nTextNode(i18nKey);
+            this._appendChild(this._textNode);
+
+            //events
+            this._onClick = new SmartJs.Event.Event(this);
+            this._addDomListener(this._dom, 'click', this._clickHandler);
+            this._btnListener = this._addDomListener(this._dom, 'touchstart', function (e) { }, { cancelBubble: true, stopPropagation: false, systemAllowed: true });   //allow system events to show css (pressed) on TabPages
+            //this._addDomListener(this._dom, 'touchend', this._clickHandler, { cancelBubble: true });//function (e) { this._dom.click(); });
         }
+
+        //events
+        Object.defineProperties(TabPage.prototype, {
+            onClick: {
+                get: function () {
+                    return this._onClick;
+                },
+            },
+        });
+
+        //properties
+        Object.defineProperties(TabPage.prototype, {
+            text: {
+                get: function () {
+                    return this._textNode.text;
+                },
+                //set: function (value) {
+                //    this._textNode.text = value;
+                //},
+            },
+            i18nKey: {
+                set: function (i18n) {
+                    this._textNode.i18n = i18n;
+                },
+            },
+            disabled: {
+                get: function () {
+                    return this._dom.disabled;
+                },
+                set: function (value) {
+                    this._dom.disabled = value;
+                },
+            },
+        });
+
+        //methods
+        TabPage.prototype.merge({
+            _clickHandler: function (e) {
+                this._dom.blur();
+                this._onClick.dispatchEvent();
+            },
+        });
 
         return TabPage;
     })(),
-
-});
+};
