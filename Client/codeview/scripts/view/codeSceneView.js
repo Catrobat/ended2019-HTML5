63cb8dadc420a114192c87f22c168cd9e092652b
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2017-07-30 18:39:59 +0200

    HTML-168 file name
    changed name: starting with lower-case letter

---
+++ Client/codeview/scripts/view/codeSceneView.js
@@ -0,0 +1,19 @@
+'use strict';
+
+PocketCode.CodeSceneView = (function () {
+    CodeSceneView.extends(PocketCode.Ui.TabPage, false);
+
+    function CodeSceneView() {
+
+    }
+
+    //properties
+    Object.defineProperties(CodeSceneView.prototype, {
+    });
+
+    //methods
+    CodeSceneView.prototype.merge({
+    });
+
+    return CodeSceneView;
+})();
