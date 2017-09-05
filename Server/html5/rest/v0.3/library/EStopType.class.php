6e51c2b30bd171b13d170daf515a3bd319f47b9c
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2017-09-05 17:12:12 +0200

    HTML-166: Fix
    changed enum values to integer

--- Server/html5/rest/v0.3/library/EStopType.class.php
+++ Server/html5/rest/v0.3/library/EStopType.class.php
@@ -1,8 +1,8 @@
 <?php
 
   abstract class EStopType {
-    const THIS_SCRIPT = "0";
-    const OTHER_SCRIPTS = "1";
-    const ALL_SOUNDS = "2";
-    const ALL = "3";
+    const THIS_SCRIPT = 0;
+    const OTHER_SCRIPTS = 1;
+    const ALL_SOUNDS = 2;
+    const ALL = 3;
   }
