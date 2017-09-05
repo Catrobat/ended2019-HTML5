79219a266b6a72f40fff173adeb0438e5d2a8f6e
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2017-09-05 23:43:42 +0200

    Backend v0.4: stop all sounds + updates
    updated changes from v0.3 (after merge from develop)

--- Server/html5/rest/v0.4/dto/StopAllSoundsBrickDto.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class StopAllSoundsBrickDto extends BaseBrickDto {
-
-    public function __construct($commentedOut = false) {
-        parent::__construct("StopAllSounds", $commentedOut);
-    }
-}
