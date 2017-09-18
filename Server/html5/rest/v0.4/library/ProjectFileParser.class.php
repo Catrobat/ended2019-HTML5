8c2d312657886ddd8a774e9e8d940e52eaadf11e
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2017-09-18 20:48:13 +0200

    HTML-116 Update for soundIDs

--- Server/html5/rest/v0.4/library/ProjectFileParser.class.php
+++ Server/html5/rest/v0.4/library/ProjectFileParser.class.php
@@ -417,7 +417,7 @@ class ProjectFileParser
             $res = $this->findItemInArrayByUrl($url, $this->sounds, true);
             if($res === false)
             {
-                $id = $this->getNewId();
+                $id = $this->getNewId() . "_" . $this->projectId;
                 $path = $this->cacheDir . "sounds" . DIRECTORY_SEPARATOR . (string)$sound->fileName;
                 if(is_file($path))
                 {
