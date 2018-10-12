90889b369ee3f29b36b5b7ed07afce266ec3935a
Wolfgang Wintersteller <wintersw@sbox.tugraz.at>
2018-10-12 17:26:22 +0200

    catrobat file version v0.997 added
    for next release

--- Server/html5/rest/v0.3/controller/ProjectsController.class.php
+++ Server/html5/rest/v0.3/controller/ProjectsController.class.php
@@ -333,6 +333,7 @@ class ProjectsController extends BaseController
                     case 0.994:
                     case 0.995:
                     case 0.996:
+                    case 0.997:
                         $parser = new ProjectFileParser_v0_994($projectId, $resourceRoot, $cacheDir, $xml);
                         break;
                     default:
